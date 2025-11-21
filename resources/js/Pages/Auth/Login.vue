cf33de026ae45a27bc1e494f57b2133cd4e9c933
Willy0117 <t_kumoda@coo-net.co.jp>
2025-11-21 03:50:39 +0000

    etc update

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -7,6 +7,9 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
 
 defineProps({
     canResetPassword: Boolean,
@@ -30,7 +33,7 @@ const submit = () => {
 </script>
 
 <template>
-    <Head title="Log in" />
+    <Head title="{{ t('login') }}" />
 
     <AuthenticationCard>
         <template #logo>
@@ -43,7 +46,7 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel :for="'email'" :value="t('email')" />
                 <TextInput
                     id="email"
                     v-model="form.email"
@@ -57,7 +60,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel :for="password" :value="t('password')" />
                 <TextInput
                     id="password"
                     v-model="form.password"
@@ -72,17 +75,17 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ms-2 text-sm text-gray-600">{{ t('remember') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Forgot your password?
+                    {{ t('forgot_password') }}
                 </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
+                    {{ t('login') }}
                 </PrimaryButton>
             </div>
         </form>
